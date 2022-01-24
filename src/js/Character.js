7d059f90c47d4f90160c35d202c8a38ce9f11b30
Sergey <84701382+Sergl82@users.noreply.github.com>
2022-01-25 07:49:54 +1100

    ajs-classes-and-inheritance

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,26 +1,33 @@
-const HEROES_VALUES = [
-  'Bowman',
-  'Swordsman',
-  'Magician',
-  'Daemon',
-  'Undead',
-  'Zombie',
-];
 
 class Character {
-  constructor(name, type) {
-    this.name = name;
-    this.type = type;
-    this.health = 100;
-    this.level = 1;
-    this.attack = 0;
-    this.defence = 0;
 
+  static heroesValues = [
+    'Bowman',
+    'Swordsman',
+    'Magician',
+    'Daemon',
+    'Undead',
+    'Zombie',
+  ];
+
+  constructor(name, type) {
+    
     if (name.length < 2 || name.length > 10) {
       throw new Error('Имя героя должно состоять от 2 до 10 символов.');
-    } else if (!(HEROES_VALUES.includes(this.type))) {
+    }
+
+    if (!(Character.heroesValues.includes(type))) {
       throw new Error('Выбранный тип не существует.');
     }
+
+    this.name = name;
+    this.type = type;
+
+    this.health = 100;
+    this.level = 1;
+
+    this.attack = undefined;
+    this.defence = undefined;
   }
 }
 
